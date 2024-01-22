fc6edbec428cddcb1ef423ff0fde3ac0dc1fce81
Theyan <rstheyanashvaran17.22@ichat.sp.edu.sg>
2024-01-22 10:01:53 +0800

    button press for patient sign up page

--- app/src/main/java/com/sp/healthease/Registration.java
+++ app/src/main/java/com/sp/healthease/Registration.java
@@ -1,12 +1,13 @@
 package com.sp.healthease;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.material.button.MaterialButton;
 import java.io.IOException;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
@@ -21,5 +22,15 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        MaterialButton doctorbutton = findViewById(R.id.Doc_signup);
+        MaterialButton patientbutton = findViewById(R.id.Patient_signup);
+
+        patientbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Registration.this,Signup_patient_1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
