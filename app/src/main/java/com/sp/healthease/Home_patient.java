674997af442701efb9462ac0f2ec4e39ad7a0fef
Theyan <rstheyanashvaran17.22@ichat.sp.edu.sg>
2024-01-28 14:15:20 +0800

    appointFragment

--- app/src/main/java/com/sp/healthease/Home_patient.java
+++ app/src/main/java/com/sp/healthease/Home_patient.java
@@ -1,8 +1,12 @@
 package com.sp.healthease;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import androidx.cardview.widget.CardView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Home_patient extends AppCompatActivity {
     @Override
@@ -10,6 +14,4 @@ public class Home_patient extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage_patient);
     }
-
-
 }
