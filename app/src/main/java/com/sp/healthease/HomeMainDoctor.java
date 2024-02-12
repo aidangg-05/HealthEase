4e03db6397ba0a40e75f3e33d34ffe1e730d8359
Theyan <rstheyanashvaran17.22@ichat.sp.edu.sg>
2024-02-13 00:07:20 +0800

    added telegram and updated home_main_doc

--- app/src/main/java/com/sp/healthease/HomeMainDoctor.java
+++ app/src/main/java/com/sp/healthease/HomeMainDoctor.java
@@ -1,5 +1,11 @@
 package com.sp.healthease;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.cardview.widget.CardView;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,8 +14,7 @@ import android.view.ViewGroup;
 
 
 public class HomeMainDoctor extends Fragment {
-
-
+    CardView telegramBtn;
     public HomeMainDoctor() {
     }
 
@@ -22,6 +27,40 @@ public class HomeMainDoctor extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_main_doctor, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_main_doctor, container, false);
+        telegramBtn = view.findViewById(R.id.telegramCardView);
+
+        // Set click listener for the Telegram button
+        telegramBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTelegram();
+            }
+        });
+        return view;
     }
+    // Method to open Telegram app
+    private void openTelegram() {
+        // Check if Telegram is installed on the device
+        if (isTelegramInstalled()) {
+            // If Telegram is installed, create an Intent to launch it
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://telegram.me/"));
+            startActivity(intent);
+        } else {
+            // If Telegram is not installed, display a toast message
+            Toast.makeText(getActivity(), "Telegram is not installed on your device", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Method to check if Telegram is installed on the device
+    private boolean isTelegramInstalled() {
+        PackageManager pm = getActivity().getPackageManager();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://telegram.me/"));
+
+        // Check if there's any app that can handle the Intent
+        return intent.resolveActivity(pm) != null;
+    }
+
 }
